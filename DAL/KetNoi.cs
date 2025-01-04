7fd2130adbfbd5c4c94b23d40322ebafa11682d9
MEEP-D <acduong4567@gmail.com>
2025-01-04 21:41:49 +0700

    Add project files.

---
+++ DAL/KetNoi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class KetNoi
+    {
+        public SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-3913IU8\PHUONG2003;Initial Catalog=QUANLYNHANVIEN;Integrated Security=True;");
+        public void CheckConnection()
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+    }
+}
